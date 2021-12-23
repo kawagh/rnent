279df024d29e049de9cd0441943d6c3982a4b2f3
kawagh <kawagh@example.com>
2021-12-23 21:01:11 +0900

    println file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+extern crate clap;
+use std::{fs::File, io::BufRead, io::BufReader};
+
+use clap::App;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let matches = App::new("rnent")
+        .version("1.0")
+        .author("kawagh")
+        .about("Tool to write note")
+        .get_matches();
+
+    for line in BufReader::new(File::open("test.md")?).lines() {
+        let l = line?;
+        println!("{}", l);
+    }
+    Ok(())
 }
---
+++ test.md
@@ -0,0 +1,6 @@
+# tag1
+- content 1-1
+- content 1-2
+# tag2
+- content 2-1
+- content 2-2
